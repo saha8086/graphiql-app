2c61cb20b3124cf12b19cda35b09a46941b77491
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-05-10 20:26:08 +0300

    init: support latest Chrome version

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), legacy({ targets: 'last 1 Chrome major version' })],
   test: {
     globals: true,
     environment: 'jsdom',
